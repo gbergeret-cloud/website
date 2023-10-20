56ef782f2a0fc3324e7ff34ce11a1ca58b6861bb
Gautier Briard <gautier@hey.com>
2023-10-20 08:56:45 +0300

    improve the shortcut preview teaser code for dev

--- assets/js/shortcut-preview.js
+++ assets/js/shortcut-preview.js
@@ -6,6 +6,21 @@ const pwd = "Control" + "devmode" + "Shift";
 
 let buffer = "";
 
+const isLocalEnv = window.location.href.includes("0.0.0.0:4000");
+
+const showDevMode = () => {
+  previewElement.forEach((el) => {
+    el.classList.remove("preview");
+  });
+  commingSoonElements.forEach((el) => {
+    el.style.display = "none";
+  });
+};
+
+if (isLocalEnv) {
+  showDevMode();
+}
+
 document.addEventListener("keydown", function (event) {
   if (buffer.length > pwd.length) {
     buffer = "";
@@ -16,13 +31,7 @@ document.addEventListener("keydown", function (event) {
     buffer += event.key;
   }
 
-  console.log(buffer);
   if (buffer === pwd) {
-    previewElement.forEach((el) => {
-      el.classList.remove("preview");
-    });
-    commingSoonElements.forEach((el) => {
-      el.style.display = "none";
-    });
+    showDevMode();
   }
 });
