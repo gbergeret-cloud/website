0910469a602b78165bf36cbfa19c833409fa595b
Gautier Briard <gautier@hey.com>
2023-10-23 00:07:48 +0300

    add carrousel behavior on logos

---
+++ assets/js/swiper-init.js
@@ -0,0 +1,26 @@
+window.onload = () => {
+  const swiper = new Swiper(".swiper", {
+    grabCursor: true,
+    slidesPerView: 1, //default value
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: true,
+    },
+    loop: true,
+    freeMode: true,
+    speed: 5000,
+    freeModeMomentum: false,
+    breakpoints: {
+      // when window width is >= 320px
+      425: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      1024: {
+        slidesPerView: 5, //this value should be maximum half of the number of element in the list
+      },
+    },
+  });
+};
