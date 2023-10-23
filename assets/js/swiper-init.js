cc1b718c2a2909d9af67d9277fed65c4283be970
Gautier Briard <gautier@hey.com>
2023-10-23 10:54:51 +0300

    add carrousel for certifications

--- assets/js/swiper-init.js
+++ assets/js/swiper-init.js
@@ -1,7 +1,7 @@
 const swiperInit = () => {
-  const swiper = new Swiper(".swiper", {
+  new Swiper("#trusted-by .swiper", {
     grabCursor: true,
-    slidesPerView: 1, //default value
+    slidesPerView: 1, // default value
     autoplay: {
       delay: 1,
       disableOnInteraction: true,
@@ -19,7 +19,32 @@ const swiperInit = () => {
         slidesPerView: 4,
       },
       1024: {
-        slidesPerView: 5, //this value should be maximum half of the number of element in the list
+        slidesPerView: 5, // this value should be maximum half of the number of element in the list
+      },
+    },
+  });
+
+  new Swiper("#certifications.swiper", {
+    grabCursor: true,
+    slidesPerView: 1, // default value
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: true,
+    },
+    loop: true,
+    freeMode: true,
+    speed: 5000,
+    freeModeMomentum: false,
+    breakpoints: {
+      // when window width is >= 320px
+      425: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      1024: {
+        slidesPerView: 4, // this value should be maximum half of the number of element in the list
       },
     },
   });
